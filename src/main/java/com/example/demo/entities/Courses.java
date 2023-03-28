9efbcfbe316c75ac93e221c619a78d3b995833c7
Ashutosh3234 <udhanashutosh@gmail.com>
2023-03-29 00:02:56 +0530

    message

---
+++ src/main/java/com/example/demo/entities/Courses.java
@@ -0,0 +1,49 @@
+package com.example.demo.entities;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Document(collection = " courses")
+public class Courses {
+	@Id
+	private long id;
+	private String title;
+	private String discription;
+	public Courses(long id, String title, String discription) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.discription = discription;
+	}
+	public Courses() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDiscription() {
+		return discription;
+	}
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	@Override
+	public String toString() {
+		return "Courses [id=" + id + ", title=" + title + ", discription=" + discription + "]";
+	}
+	
+	
+
+}
